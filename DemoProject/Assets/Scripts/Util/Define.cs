af3d8829dea1b5e3faba16398b03526ba8e562b2
namgyeongtae <gotsuebel@gmail.com>
2024-07-07 21:18:44 +0900

    Add Util Functions

---
+++ DemoProject/Assets/Scripts/Util/Define.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Define
+{
+    public enum UIEvent
+    {
+        Click,
+        Pressed,
+        PointerUp,
+        PointerDown,
+        Drag,
+        BeginDrag,
+        EndDrag
+    }
+
+    public enum MouseEvent
+    {
+        Press,
+        Click,
+    }
+}
