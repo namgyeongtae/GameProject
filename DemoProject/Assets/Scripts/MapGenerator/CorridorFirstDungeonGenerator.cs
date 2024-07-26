939f59d9e309cc42083d753c9ffbd8d1910ac8fe
namgyeongtae <gotsuebel@gmail.com>
2024-07-26 20:40:27 +0900

    Add rooms to each dead ends corridor

--- DemoProject/Assets/Scripts/MapGenerator/CorridorFirstDungeonGenerator.cs
+++ DemoProject/Assets/Scripts/MapGenerator/CorridorFirstDungeonGenerator.cs
@@ -22,14 +22,52 @@ public class CorridorFirstDungeonGenerator : SimpleRandomWalkDungeonGenerator
         HashSet<Vector2Int> floorPositions = new HashSet<Vector2Int>();
         HashSet<Vector2Int> potentialRoomPositions = new HashSet<Vector2Int>();
 
+        // Corridors 생성
         CreateCorridors(floorPositions, potentialRoomPositions);
 
+        // room이 시작될 수 있는 포지션에 rooomPercent 만큼 room 생성
         HashSet<Vector2Int> roomPositions = CreateRooms(potentialRoomPositions);
 
+        // 끝 쪽에 방이 없는 corridor의 floor 위치들 탐색
+        List<Vector2Int> deadEnds = FindAllDeadEnds(floorPositions);
+
+        // 끝 쪽에 Room이 없는 Corridor의 위치에 방추가
+        CreateRoomsAtDeadEnd(deadEnds, roomPositions);
+
+        // 여태껏 만든 corridor들과 room 들을 결합
         floorPositions.UnionWith(roomPositions);
 
         _tilemapVisualizer.PaintFloorTiles(floorPositions);
         WallGenerator.CreateWalls(floorPositions, _tilemapVisualizer);
+    }
+
+    private void CreateRoomsAtDeadEnd(List<Vector2Int> deadEnds, HashSet<Vector2Int> roomFloors)
+    {
+        foreach (var position in deadEnds)
+        {
+            if (!roomFloors.Contains(position))
+            {
+                var room = RunRandomWalk(randomWalkParameters, position);
+                roomFloors.UnionWith(room);
+            }
+        }
+    }
+
+    private List<Vector2Int> FindAllDeadEnds(HashSet<Vector2Int> floorPositions)
+    {
+        List<Vector2Int> deadEnds = new List<Vector2Int>();
+        foreach(var position in floorPositions)
+        {
+            int neighborsCount = 0;
+            foreach(var direction in Direction2D.cardinalDirectionList)
+            {
+                if (floorPositions.Contains(position + direction))
+                    neighborsCount++;
+            }
+            if (neighborsCount == 1)
+                deadEnds.Add(position);
+        }
+        return deadEnds;
     }
 
     private HashSet<Vector2Int> CreateRooms(HashSet<Vector2Int> potentialRoomPositions)
