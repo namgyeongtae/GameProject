58f18c2089a86abe48c05b9574c34a3c72593936
namgyeongtae <gotsuebel@gmail.com>
2024-08-19 22:56:23 +0900

    Add Player & Slime Stat Data SO

--- DemoProject/Assets/Scripts/Data/Stat.cs
+++ DemoProject/Assets/Scripts/Data/Stat.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-[CreateAssetMenu(fileName = "Stats_", menuName = "Stat/StatData")]
+[CreateAssetMenu(fileName = "Stats_", menuName = "Stat/Stats")]
 public class Stat : ScriptableObject
 {
     public float hp, attack, defense;
