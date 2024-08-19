4d1e4aec761ce9a9d3ab7c3fd8b69ad67dc41518
namgyeongtae <gotsuebel@gmail.com>
2024-08-19 23:02:03 +0900

    Add Two const define number

--- DemoProject/Assets/Scripts/Util/Define.cs
+++ DemoProject/Assets/Scripts/Util/Define.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class Define
 {
+    public static float KNOCKBACK_FORCE = 5.0f;
+    public static float INVINCIBLE_TIME = 1.0f;
+
     public enum UIEvent
     {
         Click,
