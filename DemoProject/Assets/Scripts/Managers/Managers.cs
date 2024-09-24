a01f336ac6e0d353bd64bd95e82840f5d55797e0
namgyeongtae <gotsuebel@gmail.com>
2024-09-24 20:25:13 +0900

    Register TilemapManager to Singleton

--- DemoProject/Assets/Scripts/Managers/Managers.cs
+++ DemoProject/Assets/Scripts/Managers/Managers.cs
@@ -8,18 +8,21 @@ public class Managers
     private static ResourceManager _resource = new ResourceManager();
     private static PoolManager _pool = new PoolManager();
     private static UIManager _ui = new UIManager();
-    private static CameraManager _camera;
+    private static CameraManager _camera = new CameraManager();
+    private static TilemapManager _tileMap = new TilemapManager();
 
     public static InputManager Input { get { return _input; } }
     public static ResourceManager Resource { get { return _resource; } }
     public static PoolManager Pool { get { return _pool; } }
     public static UIManager UI { get { return _ui; } }
     public static CameraManager Camera { get { return _camera;} }
+    public static TilemapManager TileMap { get { return _tileMap; } }
 
     public void Init()
     {
         _input.Init();
         _pool.Init();
+        _tileMap.Init();
     }
 
     public void Clear()
